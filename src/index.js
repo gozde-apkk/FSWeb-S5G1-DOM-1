eaee601897a6c1dbf6e768f9d3fb0975a16dd7d4
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-10 19:15:51 +0300

    S5G1 TAMAMLANDI

--- src/index.js
+++ src/index.js
@@ -43,3 +43,46 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+
+const navkismi = document.querySelectorAll("nav a");
+
+navkismi.forEach((item) => item.className="italic");
+  navkismi[0].textContent=siteContent["nav"]["nav-item-1"];
+  navkismi[1].textContent=siteContent["nav"]["nav-item-2"];
+  navkismi[2].textContent=siteContent["nav"]["nav-item-3"];
+  navkismi[3].textContent=siteContent["nav"]["nav-item-4"];
+  navkismi[4].textContent=siteContent["nav"]["nav-item-5"];
+  navkismi[5].textContent=siteContent["nav"]["nav-item-6"];
+
+document.querySelector(".cta h1").textContent=siteContent["cta"]["h1"]
+document.querySelector(".cta button").textContent=siteContent["cta"]["button"]
+document.querySelector("#logo-img").src=siteContent["images"]["logo-img"]
+document.querySelector("#cta-img").src=siteContent["images"]["cta-img"]
+
+const mainkismi=document.querySelectorAll(".text-content h4")
+mainkismi[0].textContent=siteContent["ana-içerik"]["özellikler-h4"];
+mainkismi[1].textContent=siteContent["ana-içerik"]["hakkımızda-h4"];
+mainkismi[2].textContent=siteContent["ana-içerik"]["servisler-h4"];
+mainkismi[3].textContent=siteContent["ana-içerik"]["ürünler-h4"];
+mainkismi[4].textContent=siteContent["ana-içerik"]["vizyon-h4"];
+document.querySelector("#middle-img").src = siteContent["images"]["accent-img"];
+
+const mainicerik=document.querySelectorAll(".text-content p");
+mainicerik[0].textContent=siteContent["ana-içerik"]["özellikler-içerik"];
+mainicerik[1].textContent=siteContent["ana-içerik"]["hakkımızda-içerik"];
+mainicerik[2].textContent=siteContent["ana-içerik"]["servisler-içeriği"];
+mainicerik[3].textContent=siteContent["ana-içerik"]["ürünler-içeriği"];
+mainicerik[4].textContent=siteContent["ana-içerik"]["vizyon-içeriği"];
+
+const sonkisim=document.querySelector(".contact h4");
+sonkisim.textContent=siteContent["iletisim"]["iletişim-h4"]
+
+const diger=document.querySelectorAll(".contact p")
+diger[0].textContent=siteContent["iletisim"]["adres"]
+diger[1].textContent=siteContent["iletisim"]["telefon"]
+diger[2].textContent=siteContent["iletisim"]["email"]
+
+const copy=document.querySelectorAll("footer a");
+copy.forEach((foot)=>foot.className="bold")
+copy[0].textContent=siteContent["footer"]["copyright"]
